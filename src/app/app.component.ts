ac7e3a367aca5d384dd20c4db60e571ce3304b47
Josiah Watts <josiahwatts@Josiahs-MBP.attlocal.net>
2021-01-22 01:51:11 -0500

    Make some responsive improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'voice-graph';
+  public mobileMenuOpen: boolean = false;
+
+  public toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 }
